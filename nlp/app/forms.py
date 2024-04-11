e8b8276c4be4d03b4366d387c5c2fb49e7880e25
joey <jwagner3@tulane.edu>
2024-04-11 02:30:22 -0500

    front-end updates

--- nlp/app/forms.py
+++ nlp/app/forms.py
@@ -1,12 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, FloatField, SelectField
+from wtforms.validators import DataRequired, NumberRange
 
 class MyForm(FlaskForm):
-	class Meta:  # Ignoring CSRF security feature.
-		csrf = False
+    class Meta:  # Ignoring CSRF security feature.
+        csrf = False
 
-	input_field = StringField(label='input headline:', id='input_field',
-							  validators=[DataRequired()], 
-							  render_kw={'style': 'width:50%'})
-	submit = SubmitField('Submit')
+    input_field = StringField(label='Input:', id='input_field',
+                              validators=[DataRequired()],
+                              render_kw={'style': 'width:50%'})
+
+    magnitude = FloatField(
+        label='Magnitude:',
+        validators=[DataRequired(), NumberRange(min=-3, max=3)],
+        render_kw={'style': 'width:20%', 'step': '0.1'}  # Adjust 'step' for desired increment
+    )
+
+    text_options = SelectField(label='Select Option:',
+        choices=[
+            ('option1', 'Option 1 Text'),
+            ('option2', 'Option 2 Text'),
+            ('option3', 'Option 3 Text'),
+        ],
+        validators=[DataRequired()],
+        render_kw={'class': 'text-options-dropdown'}
+    )
+
+    submit = SubmitField('Submit')
--- nlp/nlp.py
+++ nlp/nlp.py
@@ -1,3 +1,37 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import click
+import glob
+import pickle
+import sys
+
+import numpy as np
+import pandas as pd
+import re
+import requests
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score, classification_report
+
+from . import clf_path, config
+
+@click.group()
+def main(args=None):
+    """Console script for nlp."""
+    return 0
+
+@main.command('web')
+@click.option('-p', '--port', required=False, default=5000, show_default=True, help='port of web server')
+def web(port):
+    """
+    Launch the flask web app.
+    """
+    from .app import app
+    app.run(host='0.0.0.0', debug=True, port=port)
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
