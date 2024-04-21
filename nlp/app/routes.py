039099b1895f1be9d3d87764f0e63eb4459b6196
Hayden Outlaw <haydenkoutlaw@gmail.com>
2024-04-21 12:50:38 -0500

    loading model, control vectors, generating query

--- nlp/app/routes.py
+++ nlp/app/routes.py
@@ -29,6 +29,8 @@ tokenizer.pad_token_id = 0
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
 model = model.to("cuda:0" if torch.cuda.is_available() else "cpu")
 
+model = ControlModel(model, list(range(-5, -18, -1)))
+user_tag, asst_tag = "[INST]", "[/INST]"
 
 
 
@@ -44,7 +46,37 @@ def index():
 	form = MyForm()
 	result = None
 	if form.validate_on_submit():
+		vector_path = form.text_options.data
+
+		control_vector = torch.load(vector_path)
+
 		input_field = form.input_field.data
+
+		input_query = user_tag + input_field + asst_tag
+
+		print("magnitude: %s" % form.magnitude.data)
+
+		mag = float(form.magnitude.data)
+
+		input_ids = tokenizer(input_query, return_tensors="pt").to(model.device)
+		settings = {
+			"pad_token_id": tokenizer.eos_token_id, # silence warning
+			"do_sample": False, # temperature=0
+			"max_new_tokens": 512,
+			"repetition_penalty": 1.1, # reduce control jank
+		}
+
+		print("==baseline")
+		model.reset()
+		default_output = str(tokenizer.decode(model.generate(**input_ids, **settings).squeeze()))
+
+
+		print("\n++control")
+		# add the control vector with a certain strength (try increasing or decreasing this!)
+		model.set_control(control_vector, mag)
+		control_output = str(tokenizer.decode(model.generate(**input_ids, **settings).squeeze()))
+
+		model.reset()
 		#X = vec.transform([input_field])
 		#pred = clf.predict(X)[0]
 		pred = "PRED"
@@ -52,6 +84,6 @@ def index():
 		proba = 0.5
 		# flash(input_field)
 		return render_template('myform.html', title='', form=form, 
-								prediction=labels[pred], confidence='%.2f' % proba)
+								prediction=control_output, confidence='%.2f' % proba)
 		#return redirect('/index')
 	return render_template('myform.html', title='', form=form, prediction=None, confidence=None)
