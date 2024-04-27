c996f079dac6ceb3556ab69cb344e61d13099442
hayden outlaw <haydenkoutlaw@gmail.com>
2024-04-27 10:03:18 -0500

    setting up web page

--- nlp/app/routes.py
+++ nlp/app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, session
+from traitlets import default
+from flask import render_template, flash, redirect, session, url_for
 from . import app
 from .forms import MyForm
 from .. import clf_path
@@ -18,6 +19,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 sys.path.append(os.path.join(os.getcwd(), "./libraries/repeng"))
 from repeng import ControlVector, ControlModel, DatasetEntry
 
+
+
 # specify model
 model_name = "mistralai/Mistral-7B-Instruct-v0.1"
 
@@ -30,8 +33,8 @@ model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float
 model = model.to("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model = ControlModel(model, list(range(-5, -18, -1)))
-user_tag, asst_tag = "[INST]", "[/INST]"
 
+user_tag, asst_tag = "[INST]", "[/INST]"
 
 
 #clf, vec = pickle.load(open(clf_path, 'rb'))
@@ -44,16 +47,29 @@ user_tag, asst_tag = "[INST]", "[/INST]"
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 	form = MyForm()
-	result = None
+
 	if form.validate_on_submit():
+		
 		vector_path = form.text_options.data
 
 		control_vector = torch.load(vector_path)
 
+		vector_name = os.path.splitext(os.path.basename(vector_path))[0]
+
+
+		print(os.getcwd())
+		image_path_mean = url_for('static', filename=f'images/{vector_name}_mean_values.png')
+		image_path_trans = url_for('static', filename=f'images/{vector_name}_mean_values_trans.png')
+
+		print(os.path.exists(image_path_mean))
+		print(os.path.exists(image_path_trans))
+
+		
 		input_field = form.input_field.data
 
 		input_query = user_tag + input_field + asst_tag
 
+		
 		print("magnitude: %s" % form.magnitude.data)
 
 		mag = float(form.magnitude.data)
@@ -78,8 +94,13 @@ def index():
 
 		model.reset()
 		#proba = clf.predict_proba(X)[0].max()
-		# flash(input_field)
+		flash(input_field)
+  
+		
+		default_output = "default_output"
+		control_output = "control_output"
+	
 		return render_template('myform.html', title='', form=form, 
-								default = default_output, control = control_output)
+								default = default_output, control = control_output, image1 = image_path_mean, image2 = image_path_trans)
 		#return redirect('/index')
-	return render_template('myform.html', title='', form=form, default = None, control = None)
+	return render_template('myform.html', title='', form=form, default = None, control = None, image1 = None, image2 = None)
